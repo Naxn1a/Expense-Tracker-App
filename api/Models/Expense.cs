212307d06878058b4ca00b55573b6a45a7e40b53
naxn1a <persecond.sp@gmail.com>
2024-05-07 15:27:08 +0700

    fix user model

--- api/Models/Expense.cs
+++ api/Models/Expense.cs
@@ -9,7 +9,6 @@ namespace api.Models
         public string Name { get; set; } = null!;
         public decimal Amount { get; set; }
         public DateTime Date { get; set; }
-        public int UserId { get; set; }
         public User User { get; set; } = null!;
     }
 }
